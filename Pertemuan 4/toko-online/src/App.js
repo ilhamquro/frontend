76e662b555f5b378a337ed5d2047a7f21fd21dfb
ilhamquro <websitetuntong@gmail.com>
2024-03-27 09:09:22 +0700

    pertemuan 4

---
+++ Pertemuan 4/toko-online/src/App.js
@@ -0,0 +1,16 @@
+import Footer from '../../../Pertemuan 3/movie-cinema/src/component/Footer';
+import Main from '../../../Pertemuan 3/movie-cinema/src/component/Main';
+import Header from '../../../Pertemuan 3/movie-cinema/src/component/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
